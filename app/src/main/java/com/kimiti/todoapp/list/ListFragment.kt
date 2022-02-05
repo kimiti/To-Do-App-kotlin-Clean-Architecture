6e9b699c94938ce09e8c474a52be47bec47bc5b8
kevin <kimitikevin@gmail.com>
2022-02-05 14:45:14 +0300

    first commit

--- app/src/main/java/com/kimiti/todoapp/list/ListFragment.kt
+++ app/src/main/java/com/kimiti/todoapp/list/ListFragment.kt
@@ -5,17 +5,32 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.kimiti.todoapp.R
+import com.kimiti.todoapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
 
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+
+        binding.floatingActionButton.setOnClickListener {
+            findNavController().navigate(R.id.action_listFragment_to_addFragment)
+        }
+
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 
 
